cab062c56135f440214ffba6c9f9945bd23cc74e
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-03-05 15:49:52 +0000

    added info section

---
+++ src/components/aboutSunny.jsx
@@ -0,0 +1,22 @@
+function SunnyDetails() {
+  return (
+    <section className="sunnyDetailsSection">
+      <div className="detailsLeft">
+        <div className="contentLeft">
+          <div className="detailsLeftContent">
+            <h3>Content Left </h3>
+          </div>
+        </div>
+      </div>
+      <div className="detailsRight">
+        <div className="contentRight">
+          <div className="detailsRightContent">
+            <h3>Content Right </h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SunnyDetails;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import Hero from "../components/hero";
 import Herobanner from "../components/herobanner";
+import SunnyDetails from "../components/aboutSunny";
 
 function Home() {
   return (
     <div>
       <Herobanner />
       <Hero />
+      <SunnyDetails />
     </div>
   );
 }
