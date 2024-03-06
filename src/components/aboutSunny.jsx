09f8dcd2ed30c301617e9bbb752b382ed12524c7
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-03-06 12:44:57 +0000

    updated content

--- src/components/aboutSunny.jsx
+++ src/components/aboutSunny.jsx
@@ -6,18 +6,23 @@ function SunnyDetails() {
           <div className="detailsLeftContent">
             <h3>About Sunny Solar Estimates </h3>
             <ul>
-              <li>The solar savings estimate are based on many assumptions.</li>
               <li>
-                The estimate is based on the assumption that you will buy the
-                system rather than lease it.
+                Sunny retrieves data from the EU Photovolatic Geographical
+                Information System (PVGIS)
+                <img className="eulogo" src="/assets/eu.svg" alt="PVGIS" />
+              </li>
+              <li>
+                Sunny assumes installed peak power of 3.5kWp. This is what the
+                manufacturer declares that the solar panels can produce under
+                standard conditions.
               </li>
               <li>
                 The estimate is based on the assumption that you will buy the
                 system rather than lease it.
               </li>
               <li>
-                The estimate is based on the assumption that you will buy the
-                system rather than lease it.
+                Sunny assumes that the panels will loose about 20% of their
+                capacity.
               </li>
             </ul>
             <div className="marketingBlock"></div>
