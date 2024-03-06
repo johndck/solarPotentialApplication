795c88a12be6ec2e8397e6efb7ccdc8ad4b379ca
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-03-06 12:46:13 +0000

    small bug fix

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -44,7 +44,7 @@ function Footer() {
 
           <div className="footerLogo">
             <img src="/assets/Sunny.svg" alt="Image 1" />
-            <div>version 0.92 6 Mar 23</div>
+            <div>version 0.93 6 Mar 23</div>
           </div>
         </div>
       </div>
